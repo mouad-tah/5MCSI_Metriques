eff5604c53b5695e3efb3e2ab660e2be8f50040a
mouad-tah <61144935+mouad-tah@users.noreply.github.com>
2024-02-12 10:37:31 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -30,6 +30,10 @@ def meteo():
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
-  
+
+@app.route("/histogramme/")
+def mongraphiquecol():
+    return render_template("graphiquecolonnes.html")
+
 if __name__ == "__main__":
   app.run(debug=True)
